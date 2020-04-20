46c37b5a64a6719702f7ab5920aad5e6b560307c
Octavian <icaracioni7@gmail.com>
2020-04-20 23:26:15 +0300

    nigga

--- Assets/Scripts/Movement/CameraFollow.cs
+++ Assets/Scripts/Movement/CameraFollow.cs
@@ -3,17 +3,26 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float offview = 0.5f;
+    public float offview = 2f;
+    public Transform mainCamera;
 
     public float smoothSpeed = 0.125f;
 
-    Vector3 offset;
-
+    Vector3 offset = new Vector3(0f, 0.5f, 0f);
 
     private void Update()
     {
-        Vector3 forward = transform.forward;
-        Vector3 right = transform.right;
+        if (Input.GetKeyDown("v"))
+        {
+            offview = -offview;
+            offset.x = offview;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 forward = target.forward;
+        Vector3 right = target.right;
 
         forward.y = 0f;
         right.y = 0f;
@@ -21,13 +30,7 @@ public class CameraFollow : MonoBehaviour
         forward.Normalize();
         right.Normalize();
 
-        if (Input.GetKeyDown("v"))
-        {
-            offview = -offview;
-            offset.x = offview;
-        }
-
-        Vector3 desiredPosition = target.position + offset.x * right - offset.z * forward;
+        Vector3 desiredPosition = target.position + offset.x * right - offset.z * forward + offset.y * new Vector3(1f, 1f, 1f);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
