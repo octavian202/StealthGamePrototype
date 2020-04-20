77b6c2c9573a6622f4d1712603abe317fe20ec95
Octavian <icaracioni7@gmail.com>
2020-04-20 23:26:15 +0300

    nigga

--- Assets/Scripts/Movement/MouseLook.cs
+++ Assets/Scripts/Movement/MouseLook.cs
@@ -21,7 +21,7 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 70f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
