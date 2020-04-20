fcabe731faea7ed250a5433563923b235596d5af
Octavian <icaracioni7@gmail.com>
2020-04-20 20:43:55 +0300

    lucrez la schimbarea partii de pe care vezi caracterul

--- Assets/Scripts/Movement/CameraFollow.cs
+++ Assets/Scripts/Movement/CameraFollow.cs
@@ -3,13 +3,31 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
+    public float offview = 0.5f;
 
     public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+
+    Vector3 offset;
+
 
     private void Update()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        if (Input.GetKeyDown("v"))
+        {
+            offview = -offview;
+            offset.x = offview;
+        }
+
+        Vector3 desiredPosition = target.position + offset.x * right - offset.z * forward;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
