eba905ebf9d2e17ee99c46a91e2b25730e7fac4d
Octavian <icaracioni7@gmail.com>
2020-04-21 17:03:50 +0300

    camera buna + offview -bug cand se uita in fata si se intoarce repede

---
+++ Assets/Scripts/View/Offview.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Offview : MonoBehaviour
+{
+    public Transform playerBody;
+    public Transform objLookingAt;
+    public float ofX;
+    public float moveSpeed;
+    public float height;
+    public float front;
+
+   
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            ofX = -ofX;
+        }
+
+
+        Vector3 up = new Vector3(0f, 1f, 0f);
+
+        Vector3 forward = playerBody.forward;
+        Vector3 right = playerBody.right;
+
+        forward.y = 0;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 endPos = playerBody.position + ofX * right + height * up + front * forward;
+        transform.position = Vector3.Lerp(transform.position, endPos, moveSpeed);
+
+    }
+}
