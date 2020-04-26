9f9685f0247cd5ce0611e9f01ec682d3e6610979
Octavian <icaracioni7@gmail.com>
2020-04-26 23:15:03 +0300

    cod mai frumos din nou

--- Assets/Scripts/Movement/MovementControler.cs
+++ Assets/Scripts/Movement/MovementControler.cs
@@ -1,29 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovementControler : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 2f;
     private float currentSpeed = 0f;
-    private float speedSmoothVelocity = 0f;
+    private float speedSmoothVelocity = 0.5f;
     private float speedSmoothTime = 0.1f;
-    private float rotationSpeedX = 0.01f;
-    private float rotationSpeedY = 0.1f;
+
+    #region rotation speed
+    [Header("Rotation speed")]
+    [SerializeField] private float rotationSpeedX = 0.01f;
+    [SerializeField] private float rotationSpeedY = 0.01f;
+    #endregion
+
     private float gravity = 3f;
 
+    #region Other moves
     private bool isCrouched = false;
     private float ind = 1f;
     public bool isRunning = false;
+    #endregion
 
     private Transform mainCameraTransform;
-
     private CharacterController controller;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
-
         mainCameraTransform = Camera.main.transform;
     }
 
@@ -34,21 +37,10 @@ public class MovementControler : MonoBehaviour
 
     private void Move()
     {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            isCrouched = !isCrouched;
-        }
+        if (Input.GetKeyDown(KeyCode.C)) Crouch();
 
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            isRunning = true;
-        } else
-        {
-            isRunning = false;
-        }
-
-        Crouch();
-        Sprint();
+        if (Input.GetKey(KeyCode.LeftShift)) Sprint();
+        else stopSprint();
 
         Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
@@ -66,10 +58,7 @@ public class MovementControler : MonoBehaviour
         Vector3 desiredMoveDirectionY = (forward * movementInput.y).normalized;
         Vector3 gravityVector = Vector3.zero;
 
-        if (!controller.isGrounded)
-        {
-            gravityVector.y -= gravity;
-        }
+        if (!controller.isGrounded) gravityVector.y -= gravity;
 
         if (desiredMoveDirectionX != Vector3.zero)
         {
@@ -83,33 +72,35 @@ public class MovementControler : MonoBehaviour
         float targetSpeed = movementSpeed * movementInput.magnitude;
         currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
 
-        controller.Move(desiredMoveDirection * movementSpeed * ind * Time.deltaTime);
+        controller.Move(desiredMoveDirection * currentSpeed * ind * Time.deltaTime);
         controller.Move(gravityVector * Time.deltaTime);
     }
 
     void Crouch()
     {
-        if (isRunning && isCrouched)
-        {
-            isRunning = false;
-        }
+        isCrouched = !isCrouched;
 
-        if (isCrouched)
-        {
-            ind = 0.5f;
-        }
-        else
-        {
-            ind = 1f;
-        }
+        if (isRunning && isCrouched) isRunning = false;
+
+
+        if (isCrouched) ind = 0.5f;
+        else ind = 1f;
+
         transform.localScale = new Vector3(1f, ind, 1f);
     }
 
+    #region Sprint
     void Sprint()
     {
-        if (isRunning)
-        {
-            ind = 2f;
-        }
+        isRunning = true;
+        ind = 2f;
     }
+
+    void stopSprint()
+    {
+        isRunning = false;
+        ind = 1f;
+    }
+
+    #endregion
 }
--- Assets/Scripts/View/Offview.cs
+++ Assets/Scripts/View/Offview.cs
@@ -6,16 +6,19 @@ public class Offview : MonoBehaviour
     public Transform playerBody;
     public CinemachineFreeLook freeLookCam;
 
-    private float ofX = 2.5f;
-    private float moveSpeed = 0.125f;
-    private float height = 0.1f;
-    private float front = 2f;
+    #region Walking variables
+    [SerializeField] private float ofX = 2.5f;
+    [SerializeField] private float moveSpeed = 0.125f;
+    [SerializeField] private float height = 0.1f;
+    [SerializeField] private float front = 2f;
+    #endregion
+
     private float camFix = -35f;
     private Vector3 endPos;
-    private float sprintHeight = 1.5f;
 
-    private bool sprint = false;
+    [SerializeField] private float sprintHeight = 1.5f;
 
+    private bool isRunning = false;
     private MovementControler movementcontroler;
 
     private void Start()
@@ -25,14 +28,12 @@ public class Offview : MonoBehaviour
 
     private void Update()
     {
-        sprint = movementcontroler.isRunning;
+        isRunning = movementcontroler.isRunning;
 
         Vector3 up = new Vector3(0f, 1f, 0f);
 
-        if (sprint)
-        {
-            endPos = playerBody.position + sprintHeight * up;
-        } else
+        if (isRunning) endPos = playerBody.position + sprintHeight * up;
+        else
         {
             if (Input.GetKeyDown("v"))
             {
